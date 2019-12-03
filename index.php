cae91e934e9f07d32bfac4a3c8df4a2511516029
siddhant <siddhant7847m@gmail.com>
2019-12-03 13:49:19 +0530

    ssh test

--- index.php
+++ index.php
@@ -196,6 +196,7 @@
 						<p><?php echo $title; ?> </p>
 						<amp-carousel class="sub-cat" type="carousel" controls height="120">
 							<div class="sub-cat-inner-container">
+								<!-- Loop assigned categories -->
 								<?php foreach ($categories as $category) {
 											$cat_name = $category->name;
 											$cat_thumbnail = get_field('thumbnail', $category)['sizes']['thumbnail'];
