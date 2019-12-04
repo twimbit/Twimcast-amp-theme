cc7c6c6d79441ef2bf7138c85ac4ca845aa74cfb
siddhant <siddhant7847m@gmail.com>
2019-12-04 17:00:07 +0530

    single page dynamic content linked

---
+++ widgets/widget-suggested.php
@@ -0,0 +1,41 @@
+<div class="trending-widget explore-all">
+    <p>Suggested categories</p>
+    <amp-carousel class="sub-cat" type="carousel" controls height="120">
+        <div class="sub-cat-inner-container">
+            <a href="#" aria-label="Bussiness Model">
+                <div class="sub-cat-img">
+                    <amp-img src="https://amp.dev/static/samples/img/image1.jpg" width="160" height="110" alt="suggested thumbnail"></amp-img>
+                    <p>Category Name</p>
+                </div>
+            </a>
+            <a href="#" aria-label="Bussiness Model">
+                <div class="sub-cat-img">
+                    <amp-img src="https://amp.dev/static/samples/img/image1.jpg" width="160" height="110" alt="suggested thumbnail"></amp-img>
+                    <p>Category Name</p>
+                </div>
+            </a>
+            <a href="#" aria-label="Bussiness Model">
+                <div class="sub-cat-img">
+                    <amp-img src="https://amp.dev/static/samples/img/image1.jpg" width="160" height="110" alt="suggested thumbnail"></amp-img>
+                    <p>Category Name</p>
+                </div>
+            </a>
+            <a href="#" aria-label="Bussiness Model">
+                <div class="sub-cat-img">
+                    <amp-img src="https://amp.dev/static/samples/img/image1.jpg" width="160" height="110" alt="suggested thumbnail"></amp-img>
+                    <p>Category Name</p>
+                </div>
+            </a>
+            <a href="#" aria-label="Bussiness Model">
+                <div class="sub-cat-img">
+                    <amp-img src="https://amp.dev/static/samples/img/image1.jpg" width="160" height="110" alt="suggested thumbnail"></amp-img>
+                    <p>Category Name</p>
+                </div>
+            </a>
+        </div>
+    </amp-carousel>
+    <a href="#" class="explore-all-link">
+        <h4>Explore all <span>>></span></h4>
+    </a>
+    <hr>
+</div>
