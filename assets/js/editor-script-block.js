72002aca60ea0a643d2ef6cb53e16e6316b87fc2
siddhant <siddhant7847m@gmail.com>
2019-12-12 13:25:41 +0530

    post title limit set to 100

--- assets/js/editor-script-block.js
+++ assets/js/editor-script-block.js
@@ -4,6 +4,28 @@
  * @since 1.0.0
  */
 /* global wp */
-wp.domReady( function() {
-	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
-} );
+wp.domReady(function() {
+  wp.blocks.unregisterBlockStyle("core/button", "squared");
+  let textArea = document.querySelector(".wp-block textarea");
+  textArea.setAttribute("maxlength", "100");
+  let maxlen = textArea.getAttribute("maxlength");
+  let textAreaWrapper = document.querySelector(".wp-block");
+  let exceedText = document.createElement("h3");
+
+  /* add styles to span */
+  exceedText.style.position = "absolute";
+  exceedText.style.top = "-5px";
+  exceedText.style.fontSize = "15px";
+  exceedText.style.color = "#000";
+  exceedText.style.right = "0px";
+  exceedText.style.fontWeight = "400";
+
+  /* Adding class to element */
+  exceedText.classList = "wp-block-limit";
+
+  /* event listner on textarea */
+  textArea.addEventListener("keyup", function() {
+    textAreaWrapper.insertBefore(exceedText, textAreaWrapper.firstChild);
+    exceedText.innerHTML = textArea.value.length + "/" + maxlen;
+  });
+});
