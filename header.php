fc6060ca108e3f1934bc1eeeaa64812814cf776c
siddhant <siddhant7847m@gmail.com>
2019-12-18 13:14:12 +0530

    header navigation added for mobile view

--- header.php
+++ header.php
@@ -88,9 +88,12 @@
 </head>
 
 <body>
-	<footer class="main-footer show-mobile">
+	<header class="main-footer show-mobile">
 		<div class="footer-container">
 			<a href="<?php echo home_url(); ?>" class="footer-home footer-icon">
+				<h3>TwimCast</h3>
+			</a>
+			<a href="<?php echo home_url(); ?>" class="footer-home footer-icon" hidden>
 				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="21" height="21.988" viewBox="0 0 21 21.988">
 					<defs>
 						<clipPath id="clip-path">
@@ -103,20 +106,36 @@
 				</svg>
 
 			</a>
-			<a href="#" class="footer-search footer-icon">
-				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 18 18">
-					<defs>
-						<clipPath id="clip-path">
-							<path id="_Icon_Сolor" data-name="🎨 Icon Сolor" d="M17,18a.994.994,0,0,1-.707-.293l-3.4-3.395A7.91,7.91,0,0,1,8,16a8,8,0,1,1,8-8,7.909,7.909,0,0,1-1.688,4.9l3.395,3.4A1,1,0,0,1,17,18ZM8,2a6,6,0,1,0,6,6A6.007,6.007,0,0,0,8,2Z" transform="translate(0)" fill="#0d1c2e" />
-						</clipPath>
-					</defs>
-					<g id="search">
-						<path id="_Icon_Сolor-2" data-name="🎨 Icon Сolor" d="M17,18a.994.994,0,0,1-.707-.293l-3.4-3.395A7.91,7.91,0,0,1,8,16a8,8,0,1,1,8-8,7.909,7.909,0,0,1-1.688,4.9l3.395,3.4A1,1,0,0,1,17,18ZM8,2a6,6,0,1,0,6,6A6.007,6.007,0,0,0,8,2Z" fill="#0d1c2e" />
-					</g>
-				</svg>
-
-			</a>
-			<a href="#" class="footer-explore footer-icon">
+			<div class="footer-search footer-icon" style="flex: 0.5;margin-right: 20px;">
+				<a href="#" class="footer-search footer-icon" on="tap:twimcast-sidebar.toggleClass(class='show')" role="button" tabindex="1">
+					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 18 18">
+						<defs>
+							<clipPath id="clip-path">
+								<path id="_Icon_Сolor" data-name="🎨 Icon Сolor" d="M17,18a.994.994,0,0,1-.707-.293l-3.4-3.395A7.91,7.91,0,0,1,8,16a8,8,0,1,1,8-8,7.909,7.909,0,0,1-1.688,4.9l3.395,3.4A1,1,0,0,1,17,18ZM8,2a6,6,0,1,0,6,6A6.007,6.007,0,0,0,8,2Z" transform="translate(0)" fill="#0d1c2e" />
+							</clipPath>
+						</defs>
+						<g id="search">
+							<path id="_Icon_Сolor-2" data-name="🎨 Icon Сolor" d="M17,18a.994.994,0,0,1-.707-.293l-3.4-3.395A7.91,7.91,0,0,1,8,16a8,8,0,1,1,8-8,7.909,7.909,0,0,1-1.688,4.9l3.395,3.4A1,1,0,0,1,17,18ZM8,2a6,6,0,1,0,6,6A6.007,6.007,0,0,0,8,2Z" fill="#0d1c2e" />
+						</g>
+					</svg>
+				</a>
+				<a href="#" class="footer-profile footer-icon" on="tap:twimcast-sidebar.toggleClass(class='show')" role="button" tabindex="1">
+					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="23" height="16" viewBox="0 0 23 16">
+						<defs>
+							<clipPath id="clip-path">
+								<rect width="23" height="16" fill="none" />
+							</clipPath>
+							<clipPath id="clip-path-2">
+								<path id="_Icon_Сolor" data-name="🎨 Icon Сolor" d="M1.211,16A1.242,1.242,0,0,1,0,14.735V14.6a1.243,1.243,0,0,1,1.211-1.266H21.788A1.244,1.244,0,0,1,23,14.6v.136A1.242,1.242,0,0,1,21.788,16Zm0-6.666A1.243,1.243,0,0,1,0,8.068V7.932A1.242,1.242,0,0,1,1.211,6.667H21.788A1.242,1.242,0,0,1,23,7.932v.136a1.244,1.244,0,0,1-1.212,1.266Zm0-6.667A1.242,1.242,0,0,1,0,1.4V1.265A1.242,1.242,0,0,1,1.211,0H21.788A1.242,1.242,0,0,1,23,1.265V1.4a1.242,1.242,0,0,1-1.212,1.265Z" fill="#0d1c2e" />
+							</clipPath>
+						</defs>
+						<g id="menu" clip-path="url(#clip-path)">
+							<path id="_Icon_Сolor-2" data-name="🎨 Icon Сolor" d="M1.211,16A1.242,1.242,0,0,1,0,14.735V14.6a1.243,1.243,0,0,1,1.211-1.266H21.788A1.244,1.244,0,0,1,23,14.6v.136A1.242,1.242,0,0,1,21.788,16Zm0-6.666A1.243,1.243,0,0,1,0,8.068V7.932A1.242,1.242,0,0,1,1.211,6.667H21.788A1.242,1.242,0,0,1,23,7.932v.136a1.244,1.244,0,0,1-1.212,1.266Zm0-6.667A1.242,1.242,0,0,1,0,1.4V1.265A1.242,1.242,0,0,1,1.211,0H21.788A1.242,1.242,0,0,1,23,1.265V1.4a1.242,1.242,0,0,1-1.212,1.265Z" fill="#0d1c2e" />
+						</g>
+					</svg>
+				</a>
+			</div>
+			<a href="#" class="footer-explore footer-icon" hidden>
 				<svg xmlns="http://www.w3.org/2000/svg" width="15.986" height="18.329" viewBox="0 0 15.986 18.329">
 					<g id="Component_409_1" data-name="Component 409 – 1" transform="translate(1 0.164)">
 						<line id="Line_1" data-name="Line 1" y2="18.126" fill="none" stroke="#0d1c2e" stroke-width="2" />
@@ -126,20 +145,6 @@
 				</svg>
 
 			</a>
-			<a href="#" class="footer-profile footer-icon">
-				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="23" height="16" viewBox="0 0 23 16">
-					<defs>
-						<clipPath id="clip-path">
-							<rect width="23" height="16" fill="none" />
-						</clipPath>
-						<clipPath id="clip-path-2">
-							<path id="_Icon_Сolor" data-name="🎨 Icon Сolor" d="M1.211,16A1.242,1.242,0,0,1,0,14.735V14.6a1.243,1.243,0,0,1,1.211-1.266H21.788A1.244,1.244,0,0,1,23,14.6v.136A1.242,1.242,0,0,1,21.788,16Zm0-6.666A1.243,1.243,0,0,1,0,8.068V7.932A1.242,1.242,0,0,1,1.211,6.667H21.788A1.242,1.242,0,0,1,23,7.932v.136a1.244,1.244,0,0,1-1.212,1.266Zm0-6.667A1.242,1.242,0,0,1,0,1.4V1.265A1.242,1.242,0,0,1,1.211,0H21.788A1.242,1.242,0,0,1,23,1.265V1.4a1.242,1.242,0,0,1-1.212,1.265Z" fill="#0d1c2e" />
-						</clipPath>
-					</defs>
-					<g id="menu" clip-path="url(#clip-path)">
-						<path id="_Icon_Сolor-2" data-name="🎨 Icon Сolor" d="M1.211,16A1.242,1.242,0,0,1,0,14.735V14.6a1.243,1.243,0,0,1,1.211-1.266H21.788A1.244,1.244,0,0,1,23,14.6v.136A1.242,1.242,0,0,1,21.788,16Zm0-6.666A1.243,1.243,0,0,1,0,8.068V7.932A1.242,1.242,0,0,1,1.211,6.667H21.788A1.242,1.242,0,0,1,23,7.932v.136a1.244,1.244,0,0,1-1.212,1.266Zm0-6.667A1.242,1.242,0,0,1,0,1.4V1.265A1.242,1.242,0,0,1,1.211,0H21.788A1.242,1.242,0,0,1,23,1.265V1.4a1.242,1.242,0,0,1-1.212,1.265Z" fill="#0d1c2e" />
-					</g>
-				</svg>
-			</a>
+
 		</div>
-	</footer>
+	</header>
