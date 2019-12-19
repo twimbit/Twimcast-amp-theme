f5d032d9d0c2a53404f91f74faba65e9a9082d83
siddhant <siddhant7847m@gmail.com>
2019-12-19 22:09:36 +0530

    block editor image css changed

---
+++ twimcast/templates/widget-templates/list-type.php
@@ -0,0 +1,38 @@
+<div class="rending-title"><?php echo $post_title; ?></div>
+<div class="trending-excerpt"><?php echo $post_excerpt; ?></div>
+<div class="author-category">
+    <div class="author-name">
+        <?php echo $post_author; ?>
+    </div>
+    <div class="category">
+        <?php echo $post_category; ?>
+    </div>
+</div>
+<div class="date-time-type">
+    <div class="date">
+        <?php echo $post_date; ?>
+    </div>
+    <div class="divider"></div>
+    <?php $audio_url = get_field('audio_upload', $post)['url'];
+                                if ($audio_url) { ?>
+        <div class="trending-time" title="Read time">
+            <?php echo $post_readTime; ?>
+        </div>
+        <div class="trending-type">
+            <img src="<?php echo $dir_path . '/assets/images/svg/headphone.svg'; ?>" alt="">
+        </div>
+    <?php    } else if (($post_type == 'read') || ($post_type == "podcast")) { ?>
+        <div class="trending-time" title="Read time">
+            <span> <?php if (!(empty($post_readTime))) {
+                                        echo $post_readTime;
+                                    } ?></span>
+        </div>
+        <div class="trending-type">
+            min <img src="<?php echo $dir_path . '/assets/images/svg/book.svg'; ?>" alt="">
+        </div>
+    <?php } ?>
+
+    <div class="add-to-queue">
+        <img src="<?php echo $dir_path . '/assets/images/svg/queue.svg'; ?>" alt="">
+    </div>
+</div>
