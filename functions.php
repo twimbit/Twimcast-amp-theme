cecf6f4543c9f61779bb70f976601b631f9b0ded
siddhant <siddhant7847m@gmail.com>
2020-02-18 18:22:55 +0530

    custom fields visibility fixed for authors

--- functions.php
+++ functions.php
@@ -905,7 +905,9 @@ add_action('pre_get_posts', 'query_set_only_author');
 function query_set_only_author($wp_query)
 {
 	global $current_user;
-	if (is_admin() && !current_user_can('edit_others_posts')) {
+	//$pagenow holds the name of the current page being viewed
+	global $pagenow;
+	if (is_admin() && !current_user_can('edit_others_posts') && ('edit.php' == $pagenow)) {
 		$wp_query->set('author', $current_user->ID);
 		add_filter('views_edit-post', 'fix_post_counts');
 		add_filter('views_upload', 'fix_media_counts');
